8412eda734547ab66b2e34dddeac0d3d0c541f7d
basantaj <77307970+basantaj@users.noreply.github.com>
2021-01-12 22:23:27 -0300

    Agregar archivos de proyecto.

---
+++ Manager/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gruVistaPrevia = new System.Windows.Forms.GroupBox();
+            this.picPrevia = new System.Windows.Forms.PictureBox();
+            this.btnSelecFuente = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCarpetaFuente = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSelecDestino = new System.Windows.Forms.Button();
+            this.listImagenes = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblDestino = new System.Windows.Forms.Label();
+            this.gruVistaPrevia.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picPrevia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gruVistaPrevia
+            // 
+            this.gruVistaPrevia.Controls.Add(this.picPrevia);
+            this.gruVistaPrevia.Location = new System.Drawing.Point(12, 12);
+            this.gruVistaPrevia.Name = "gruVistaPrevia";
+            this.gruVistaPrevia.Size = new System.Drawing.Size(244, 215);
+            this.gruVistaPrevia.TabIndex = 0;
+            this.gruVistaPrevia.TabStop = false;
+            this.gruVistaPrevia.Text = "Vista Previa";
+            // 
+            // picPrevia
+            // 
+            this.picPrevia.Location = new System.Drawing.Point(6, 20);
+            this.picPrevia.Name = "picPrevia";
+            this.picPrevia.Size = new System.Drawing.Size(232, 189);
+            this.picPrevia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPrevia.TabIndex = 0;
+            this.picPrevia.TabStop = false;
+            // 
+            // btnSelecFuente
+            // 
+            this.btnSelecFuente.Location = new System.Drawing.Point(352, 22);
+            this.btnSelecFuente.Name = "btnSelecFuente";
+            this.btnSelecFuente.Size = new System.Drawing.Size(82, 23);
+            this.btnSelecFuente.TabIndex = 1;
+            this.btnSelecFuente.Text = "Seleccionar";
+            this.btnSelecFuente.UseVisualStyleBackColor = true;
+            this.btnSelecFuente.Click += new System.EventHandler(this.btnSelecFuente_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Carpeta Fuente:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblCarpetaFuente
+            // 
+            this.lblCarpetaFuente.AutoSize = true;
+            this.lblCarpetaFuente.Location = new System.Drawing.Point(440, 27);
+            this.lblCarpetaFuente.Name = "lblCarpetaFuente";
+            this.lblCarpetaFuente.Size = new System.Drawing.Size(87, 13);
+            this.lblCarpetaFuente.TabIndex = 2;
+            this.lblCarpetaFuente.Text = "No seleccionado";
+            this.lblCarpetaFuente.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Carpeta Destino:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSelecDestino
+            // 
+            this.btnSelecDestino.Location = new System.Drawing.Point(352, 60);
+            this.btnSelecDestino.Name = "btnSelecDestino";
+            this.btnSelecDestino.Size = new System.Drawing.Size(82, 23);
+            this.btnSelecDestino.TabIndex = 1;
+            this.btnSelecDestino.Text = "Seleccionar";
+            this.btnSelecDestino.UseVisualStyleBackColor = true;
+            this.btnSelecDestino.Click += new System.EventHandler(this.btnSelecDestino_Click);
+            // 
+            // listImagenes
+            // 
+            this.listImagenes.FormattingEnabled = true;
+            this.listImagenes.Location = new System.Drawing.Point(266, 95);
+            this.listImagenes.Name = "listImagenes";
+            this.listImagenes.Size = new System.Drawing.Size(305, 121);
+            this.listImagenes.TabIndex = 4;
+            this.listImagenes.SelectedIndexChanged += new System.EventHandler(this.listImagenes_SelectedIndexChanged);
+            // 
+            // lblDestino
+            // 
+            this.lblDestino.AutoSize = true;
+            this.lblDestino.Location = new System.Drawing.Point(440, 65);
+            this.lblDestino.Name = "lblDestino";
+            this.lblDestino.Size = new System.Drawing.Size(87, 13);
+            this.lblDestino.TabIndex = 2;
+            this.lblDestino.Text = "No seleccionado";
+            this.lblDestino.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 261);
+            this.Controls.Add(this.listImagenes);
+            this.Controls.Add(this.lblDestino);
+            this.Controls.Add(this.lblCarpetaFuente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelecDestino);
+            this.Controls.Add(this.btnSelecFuente);
+            this.Controls.Add(this.gruVistaPrevia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gruVistaPrevia.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picPrevia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gruVistaPrevia;
+        private System.Windows.Forms.Button btnSelecFuente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCarpetaFuente;
+        private System.Windows.Forms.PictureBox picPrevia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSelecDestino;
+        private System.Windows.Forms.ListBox listImagenes;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblDestino;
+    }
+}
+
---
+++ Manager/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Manager
+ {
+    
+
+    public partial class Form1 : Form
+    {
+
+       
+
+        string[] archivos;
+        Image im;
+        public Form1()
+        
+        {
+            InitializeComponent();
+           
+            this.listImagenes.MouseDoubleClick+= new MouseEventHandler(listImagenes_MouseDoubleClick);
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelecFuente_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK) {
+                lblCarpetaFuente.Text = folderBrowserDialog1.SelectedPath;
+                archivos =Directory.GetFiles(folderBrowserDialog1.SelectedPath,"*.jpg");
+               
+                for (int i = 0; i <= archivos.Length-1; i++)
+                {
+                    int index = archivos[i].LastIndexOf("\\");
+                    //Console.Write(index);
+                    if (index > 0)
+                    {
+                        archivos[i] = archivos[i].Substring(index,archivos[i].Length-index);
+                       Console.Write(archivos[i]);
+                    }
+                }
+                listImagenes.DataSource = archivos;
+                folderBrowserDialog1.Dispose();
+               
+            }
+
+        }
+
+      
+
+        private void btnSelecDestino_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                lblDestino.Text = folderBrowserDialog1.SelectedPath;
+            }
+            folderBrowserDialog1.Dispose();
+        }
+
+
+       
+        
+
+        private void listImagenes_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+            int ind = this.listImagenes.IndexFromPoint(e.Location);
+           
+            if (ind != System.Windows.Forms.ListBox.NoMatches && lblDestino.Text!="No seleccionado") 
+            {
+                
+                string img = listImagenes.SelectedItem.ToString();
+                //MessageBox.Show(lblDestino.Text + img);
+                File.Copy(lblCarpetaFuente.Text + img, lblDestino.Text + img);
+                MessageBox.Show("Copiado");
+            }
+
+        }
+        private void listImagenes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Console.Write(lblCarpetaFuente.Text + listImagenes.SelectedItem);
+            if (listImagenes.SelectedItem != null)
+            {
+                im = new Bitmap(lblCarpetaFuente.Text + listImagenes.SelectedItem);
+                picPrevia.Image = im;
+            }
+        }
+    }
+}
